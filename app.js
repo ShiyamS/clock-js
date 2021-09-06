08291246908202a992ab56c6c56ab6926ad501d5
Shiyam <shiyams315@gmail.com>
2021-09-06 21:04:19 +0530

    added digital clock

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const secondHand = document.querySelector(".second-hand");
 const minHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
 
+const day = document.querySelector(".day");
+const hour = document.querySelector(".hour");
+const minutes = document.querySelector(".minutes");
+const second = document.querySelector(".seconds");
+
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
@@ -15,6 +20,21 @@ function setDate() {
   const hours = now.getHours();
   const hoursDeg = (hours / 12) * 360 + 90;
   hourHand.style.transform = `rotate(${hoursDeg}deg)`;
-}
 
+  var days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  var dayName = days[now.getDay()];
+  day.innerHTML = `${dayName}`;
+
+  hour.innerHTML = `${hours % 12}` + ":";
+  minutes.innerHTML = `${mins}` + ":";
+  second.innerHTML = `${seconds}`;
+}
 setInterval(setDate, 1000);
